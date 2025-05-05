63fb61fa13b13cdb601ecb179e3a4604ec633d12
Max Eagan <meagan7654@gmail.com>
2025-05-04 20:30:13 -0500

    Add api endpoints

---
+++ frontend/src/services/api/apiService.ts
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+    CreateRestaurantRequest,
+    CreateReviewRequest,
+    PaginatedResponse,
+    Photo,
+    Restaurant,
+    RestaurantSearchParams,
+    RestaurantSummary,
+    Review,
+    UpdateRestaurantRequest,
+    UpdateReviewRequest,
+  } from "@/domain/domain";
+
+// Api service interface
+export interface ApiService {
+    // Restaurant endpoints
+    searchRestaurants(
+      params: RestaurantSearchParams,
+    ): Promise<PaginatedResponse<RestaurantSummary>>;
+
+    getRestaurant(restaurantId : string) : Promise<Restaurant>;
+
+    createRestaurant(request: CreateReviewRequest) : Promise<Restaurant>;
+
+    updateRestaurant(
+        restaurantId: string,
+        request: UpdateRestaurantRequest,
+    ): Promise<void>;
+
+    deleteRestaurant(restaurantId: string): Promise<void>;
+
+    getRestaurantReviews(
+        restaurantId: string,
+        sort? : "datePosted,desc" | "datePosted,asc" | "rating.desc" | "rating,asc",
+        page?: number,
+        size?: number,
+    ): Promise<PaginatedResponse<Review>>;
+
+    getRestaurantReview(restaurantId: string, reviewId: string): Promise<Review>;
+
+    createReview(
+        restaurantId: string,
+        request: CreateReviewRequest,
+    ): Promise<Review>;
+
+    updateReview(
+        restaurantId: string,
+        reviewId: string,
+        request: UpdateRestaurantRequest,
+    ): Promise<void>;
+
+    deleteReview(restaurantId: string, reviewId: string): Promise<void>;
+
+    // Photo endpoint
+    uploadPhoto(file: File, caption?: string): Promise<Photo>; 
+}
