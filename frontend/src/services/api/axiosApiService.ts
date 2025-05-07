4b0170cefde1904be56dd62aa86b473e779b40c6
Max Eagan <meagan7654@gmail.com>
2025-05-06 21:48:28 -0500

    Finish endpoints, though not sure if functional at the moment.

--- frontend/src/services/api/axiosApiService.ts
+++ frontend/src/services/api/axiosApiService.ts
@@ -22,6 +22,7 @@ import axios, {
 import { useAuth } from "react-oidc-context";
 import { ApiService } from "./apiService";
 import { error } from "console";
+import { headers } from "next/headers";
 
 export class AxiosApiService implements ApiService {
   // Axios instance to make HTTP requests
@@ -122,47 +123,125 @@ export class AxiosApiService implements ApiService {
   // Restauraunt endpoints
 
   // Endpoint to search for a restauraunt. 
+  // Using HTTP GET Method. 
   public async searchRestaurants(
     params: RestaurantSearchParams,
     ): Promise<PaginatedResponse<RestaurantSummary>>{ 
         const response: AxiosResponse<PaginatedResponse<RestaurantSummary>> =
         await this.api.get("/restaurants", { params });
     return response.data;        
-}
+    }
 
-public async getRestaurant(restaurantId: string): Promise<Restaurant> {
-}
+    // Endpoint to get restaraunt by a specific id.
+    // Using HTTP GET Method.
+    public async getRestaurant(restaurantId: string): Promise<Restaurant> {
+        const response: AxiosResponse<Restaurant> =  await this.api.get
+        (`/restaurants/${restaurantId}`);
+        return response.data;
+    }
 
-public async createRestaurant(request: CreateReviewRequest): Promise<Restaurant> {
-    
-}
+    // Endpoint to add(create) a new restaurant. 
+    // Using HTTP POST method.
+    public async createRestaurant(request: CreateReviewRequest): Promise<Restaurant> {
+        const response: AxiosResponse<Restaurant> = await this.api.post
+        ("/restaurants", request,);
+        return response.data;
+    }
 
-public async updateRestaurant(restaurantId: string, request: UpdateRestaurantRequest): Promise<void> {
-    
-}
+    // Endpoint to update restaurant.
+    // Using HTTP PUT method.
+    public async updateRestaurant(restaurantId: string, request: UpdateRestaurantRequest): Promise<void> {
+        const response: AxiosResponse<Restaurant> = 
+        await this.api.put(`/restaurants/${restaurantId}`, request);
+    }
 
-public async deleteRestaurant(restaurantId: string): Promise<void> {
-    
-}
+    // Endpoint to delete a restauraunt. 
+    // Using HTTP DELETE method. 
+    public async deleteRestaurant(restaurantId: string): Promise<void> {
+        const response: AxiosResponse<Restaurant> = 
+        await this.api.delete(`/restaurants/${restaurantId}`);
+    }
 
-public async getRestaurantReview(restaurantId: string, reviewId: string): Promise<Review> {
-    
-}
+    // Endpoint to get a reveiw for a specific restaurant. 
+    public async getRestaurantReviews(
+        restaurantId: string, 
+        sort?: "datePosted,desc" | "datePosted,asc" | "rating.desc" | "rating,asc", 
+        page?: number, 
+        size?: number):
+         Promise<PaginatedResponse<Review>> {
+            const response: AxiosResponse<PaginatedResponse<Review>> = 
+                await this.api.get(`/restauraunts/${restaurantId}/reviews`, {
+                    params: { sort, page, size},
+                });
+            return response.data;
+    }
 
-public async createReview(restaurantId: string, request: CreateReviewRequest): Promise<Review> {
-    
-}
+    // Endpoint to get a speicific review
+    public async getRestaurantReview(
+        restaurantId: string, 
+        reviewId: string): 
+        Promise<Review> {
+            const response: AxiosResponse<Review> = 
+            await this.api.get(
+            `/restaurants/${restaurantId}/reviews/${reviewId}`
+        );
+        return response.data;
+            
+    }
 
-public async updateReview(restaurantId: string, reviewId: string, request: UpdateRestaurantRequest): Promise<void> {
-    
-}
+    // Endpoint to create a review
+    public async createReview(
+        restaurantId: string, 
+        request: CreateReviewRequest): 
+        Promise<Review> {
+            const response: AxiosResponse<Review> = 
+            await this.api.post(
+                `/restaurants/${restaurantId}/reviews/`, request
+            );
+            return response.data; 
+    }
 
-public async deleteReview(restaurantId: string, reviewId: string): Promise<void> {
-    
-}
+    // Endpoint to update a review.
+    public async updateReview(
+        restaurantId: string, 
+        reviewId: string, 
+        request: UpdateRestaurantRequest): 
+        Promise<void> {
+            const response: AxiosResponse<Review> = 
+            await this.api.put(
+                `/restaurants/${restaurantId}/reviews/${reviewId}`, request
+            );
+    }
 
-public async uploadPhoto(file: File, caption?: string): Promise<Photo> {
-    
-}
+    public async deleteReview(
+        restaurantId: string, 
+        reviewId: string): 
+        Promise<void> {
+            const response: AxiosResponse<Review> = 
+            await this.api.delete(
+                `/restaurants/${restaurantId}/reviews/${reviewId}`
+            );
+    }
 
+    public async uploadPhoto(
+        file: File, 
+        caption?: string): 
+        Promise<Photo> {
+            const formData = new FormData; 
+            formData.append("file", file);
+            if (caption){
+                formData.append("caption", caption);
+            }
+
+            const response: AxiosResponse<Photo> = await.this.api.post(
+                "/photos",
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    },
+                },
+            );
+            return response.data; 
+    }
 }
