f6d0afdc75eb8570d398e15af759518bb0f4ee63
Max Eagan <meagan7654@gmail.com>
2025-06-17 18:34:12 -0500

    implement getReview in ReviewService

--- backend/restaurant/src/main/java/com/maxeagan/restaurant/services/impl/ReviewServiceImpl.java
+++ backend/restaurant/src/main/java/com/maxeagan/restaurant/services/impl/ReviewServiceImpl.java
@@ -17,10 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -101,6 +98,16 @@ public class ReviewServiceImpl implements ReviewService {
         int end = Math.min(start + pageable.getPageNumber(), reviews.size());
 
         return new PageImpl<>(reviews.subList(start,end), pageable, reviews.size());
+    }
+
+    @Override
+    public Optional<Review> getReview(String restaurantId, String reviewId) {
+        Restaurant restaurant = getRestaurantOrThrow(restaurantId);
+        
+        return restaurant.getReviews()
+                .stream()
+                .filter(r -> reviewId.equals(r.getId()))
+                .findFirst()
     }
 
     private Restaurant getRestaurantOrThrow(String restaurantId) {
